868a918d5ca3ece0f0ebfd7f75708552cfee0357
ivanlalic <ivanlalic@gmail.com>
2020-08-14 00:32:00 -0300

    Updates

---
+++ precache-manifest.dc7224d52b82c203b582d6ac9cba54b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7071405c95ad3d7b9b58d2eaa75df4e0",
+    "url": "/lyrix/index.html"
+  },
+  {
+    "revision": "a24adeae6171cdd19f12",
+    "url": "/lyrix/static/css/main.8ba7a252.chunk.css"
+  },
+  {
+    "revision": "028f5a249cf2f36c211e",
+    "url": "/lyrix/static/js/2.940f6739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrix/static/js/2.940f6739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24adeae6171cdd19f12",
+    "url": "/lyrix/static/js/main.ce8ea683.chunk.js"
+  },
+  {
+    "revision": "f745606f1802d6ebe46d",
+    "url": "/lyrix/static/js/runtime-main.c43ce210.js"
+  }
+]);
